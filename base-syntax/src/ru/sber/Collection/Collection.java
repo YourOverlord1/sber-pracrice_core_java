b9ce637256d7f4a8c969d3f72510d8caffffdf3e
YourOverlord1 <denissakolov2016@gmail.com>
2023-03-20 22:11:45 +0300

    Выполнено задание по лекции "Объектно Ориантированное програмирование часть 2"

---
+++ base-syntax/src/ru/sber/Collection/Collection.java
@@ -0,0 +1,10 @@
+package ru.sber.Collection;
+
+public interface Collection {
+    int size();
+    boolean isEmpty();
+    boolean contains(Object item);
+    boolean add(Object item);
+    boolean remove(Object item);
+    void clear();
+}
---
+++ base-syntax/src/ru/sber/Collection/Deque.java
@@ -0,0 +1,12 @@
+package ru.sber.Collection;
+import java.util.NoSuchElementException;
+public interface Deque {
+    void addFirst(Object item);
+    void addLast(Object item);
+    Object getFirst() throws NoSuchElementException;
+    Object getLast() throws NoSuchElementException;
+    Object pollFirst();
+    Object pollLast();
+    Object removeFirst() throws NoSuchElementException;
+    Object removeLast() throws NoSuchElementException;
+}
